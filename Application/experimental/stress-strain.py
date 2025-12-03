99478cb631279047e2d4c0b566d6d0033d2bff6e
tylerjacobmueller08@gmail.com <tylerjacobmueller08@gmail.com>
2025-12-03 16:52:19 -0700

    Updated stress-strain file and added an artificial dataset

--- Application/experimental/stress-strain.py
+++ Application/experimental/stress-strain.py
@@ -1,31 +1,28 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-internal_diameter = 2/32 
-wall_thickness = 1/32
+internal_diameter = 1 / 16 # Inches
+wall_thickness = 1 / 32 # Inches
+pixels_to_inches = 0.125 / 185
 
 df = pd.read_csv("data.csv")
 
-pressure = df["Pressure [PSI]"]
-diameter = df["Diameter [px]"]
+pressure = df["Pressure [PSI]"].astype(float)
+diameter = df["Diameter [px]"].astype(float) * pixels_to_inches
 initial_diameter = diameter.iloc[0]
 
 df["Stress"] = (pressure * diameter) / (2 * wall_thickness)
 df["Strain"] = (diameter - initial_diameter) / initial_diameter
 
-df.to_csv("output.csv", index=False)
+df_sorted = df.sort_values("Strain")
 
-
-df = pd.read_csv("output.csv")
-
-stress = df["Stress"]
-strain = df["Strain"]
-
-plt.figure()
-plt.plot(strain, stress, linewidth=2)
+plt.figure(figsize=(8, 5))
+plt.plot(df_sorted["Strain"], df_sorted["Stress"], linewidth=2)
 plt.xlabel("Strain")
-plt.ylabel("Stress")
+plt.ylabel("Stress [PSI]")
 plt.title("Stress-Strain Curve")
 plt.grid(True)
-
+plt.tight_layout()
 plt.show()
+
+df.to_csv("output.csv", index=False)
